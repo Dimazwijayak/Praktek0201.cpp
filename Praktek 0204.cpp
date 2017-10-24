c22318932679bf928a498c99c736632fb08db84c
Dimazwijayak <32424380+Dimazwijayak@users.noreply.github.com>
2017-10-24 19:02:48 +0700

    Pembuatan Variabel

--- Praktek 0204.cpp
+++ Praktek 0204.cpp
@@ -2,4 +2,8 @@
 using namespace std;
 
 int main(){
+	double Luas_rumah=42;
+	double  lantai_parket=193*1215;
+	double Luas_1box=lantai_parket*8/1000000;
+	double jmlh_box_parket,biaya_parket,biaya_pasang,totalbyr;
 }
